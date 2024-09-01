0042f8cb38139b0eddad3eb37fb2043f8e7596ea
Letícia <l184423@dac.unicamp.br>
2024-08-31 22:06:14 -0300

    Tarefa 2) finalizada

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Este repositório é dedicado à implementação de um pipeline para o processam
 
 * **OrthanC**
 
-* **API REST**
+* A **API REST** do Orthanc permite que a interação com o servidor para realizar operações como upload, consulta e manipulação de imagens DICOM e outros dados relacionados.
 
 * **TorchXRayVision**
 
@@ -20,6 +20,16 @@ Este repositório é dedicado à implementação de um pipeline para o processam
 
 #### Tarefa 1
 * De início, instalei o Docker Desktop e o configurei no ambiente virtual WSL2, com a ajuda do seguinte repositório: https://github.com/codeedu/wsl2-docker-quickstart.
-* A partir disso, para baixar a imagem do OrthanC, utilizei o comando `docker pull jodogne/orthanc` e segui as instruções presentes em https://orthanc.uclouvain.be/book/users/docker.html. Aprendi que, para simplificar o gerenciamento do container do OrthanC, pode-se criar os arquivos `docker-compose.yaml` e `orthanc.json`, contendo todos as configurações e plugins necessárias para rodar corretamente a aplicação. 
+* A partir disso, para baixar a imagem do OrthanC, utilizei o comando `docker pull jodogne/orthanc` e segui as instruções em https://orthanc.uclouvain.be/book/users/docker.html. Aprendi que, para simplificar o gerenciamento do container do OrthanC, pode-se criar os arquivos `docker-compose.yaml` e `orthanc.json`, contendo todos as configurações e plugins necessários para rodar corretamente a aplicação. 
 * Com isso, acessei http://localhost:8042 para interagir com o ambiente do OrthanC.
-* Minha principal dificuldade nessa tarefa foi entender a função de cada ferramenta e como elas estariam integradas.
+* Minha principal dificuldade nessa tarefa foi entender a função de cada ferramenta e como elas estariam integradas. Os links acima foram essenciais para ajudar nessa configuração.
+
+#### Tarefa 2
+* Com o auxílio do ChatGPT, criei um script (`send_dicom.py`) em python para enviar os arquivos na pasta `dicom_samples` para o OrthanC.
+* A API REST do OrthanC foi utilizada para enviar arquivos DICOM para o servidor Orthanc por meio de uma requisição HTTP, utilizando a função `requests.post()` de python.
+* Nessa tarefa, o principal desafio foi entender como funcionava a biblioteca `requests` e como utilizar a API REST a partir disso.
+* Após finalizar e rodar o script, todos os arquivos `.dcm` foram enviados com sucesso para o OrthanC, como verifiquei acessando a interface web em http://localhost:8042.
+
+#### Tarefa 3
+
+#### Tarefa 4 (EXTRA)
--- send_dicom.py
+++ send_dicom.py
@@ -1,10 +1,10 @@
-import os
+import os   
 import requests
 
 # Configurações
 ORTHANC_URL = "http://localhost:8042/instances" # Endpoint do Orthanc para envio de arquivos
-ORTHANC_USERNAME = "orthanc"  # Usuário padrão do Orthanc
-ORTHANC_PASSWORD = "orthanc"  # Senha padrão do Orthanc
+ORTHANC_USERNAME = "orthanc"                    # Usuário padrão do Orthanc
+ORTHANC_PASSWORD = "orthanc"                    # Senha padrão do Orthanc
 DICOM_DIR = os.getcwd() + "/dicom_samples"      # Diretório onde os arquivos DICOM estão armazenados
 
 def send_dicom_to_orthanc(file_path):
@@ -13,18 +13,21 @@ def send_dicom_to_orthanc(file_path):
     Args:
         file_path (str): Caminho completo do arquivo DICOM.
     """
+    file_name = os.path.basename(file_path)
     with open(file_path, 'rb') as dicom_file:
-        files = {'file': (os.path.basename(file_path), dicom_file, 'application/dicom')}
+        # O dicionário files é usado para enviar arquivos em uma requisição HTTP
+        files = {'file': (file_name, dicom_file, 'application/dicom')}
+        # Enviando o arquivo DICOM para o Orthanc usando a API REST
         response = requests.post(
-            ORTHANC_URL,
-            files=files,
-            auth=(ORTHANC_USERNAME, ORTHANC_PASSWORD)
+            ORTHANC_URL,                                # URL do Orthanc
+            files=files,                                # Arquivo DICOM a ser enviado
+            auth=(ORTHANC_USERNAME, ORTHANC_PASSWORD)   # Autenticação básica à requisição HTTP
         )
         if response.status_code == 200:
-            print(f"Arquivo {file_path} enviado com sucesso!")
+            print(f"Arquivo {file_name} enviado com sucesso!")
         else:
-            print(f"Falha ao enviar o arquivo {file_path}. Código de status: {response.status_code}")
-            print("Resposta:", response.text)
+            print(f"Falha ao enviar o arquivo {file_name}. Código de status: {response.status_code}")
+            print("Resposta: ", response.text)
 
 def main():
     # Verifica se o diretório DICOM_DIR existe
@@ -33,9 +36,9 @@ def main():
         return
 
     # Percorre todos os arquivos no diretório DICOM_DIR
-    for root, dirs, files in os.walk(DICOM_DIR):
+    for root, _, files in os.walk(DICOM_DIR):    # Percorre todos os arquivos no diretório DICOM_DIR e subdiretórios
         for file in files:
-            if file.lower().endswith(".dcm"):  # Verifica se o arquivo é um DICOM
+            if file.lower().endswith(".dcm"):    # Verifica se o arquivo é um DICOM
                 file_path = os.path.join(root, file)
                 send_dicom_to_orthanc(file_path)
 
